134d22913488a24f404ac82ad63c75b8ecd8a71f
luozhengang <939945825@qq.com>
2021-08-19 11:29:58 +0800

    ui调整

--- src/views/main.vue
+++ src/views/main.vue
@@ -8,73 +8,71 @@
       @close="handleClose">
       <el-submenu index="1">
         <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>导航一</span>
+          <i class="el-icon-aim"></i>
+          <span>过滤</span>
         </template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="1-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="1-4-1">选项1</el-menu-item>
+        <el-submenu index="1-1">
+          <template slot="title">标签</template>
+          <el-menu-item index="1-1-1">选项1</el-menu-item>
+          <el-menu-item index="1-1-2">选项2</el-menu-item>
+          <el-menu-item index="1-1-3">选项3</el-menu-item>
+        </el-submenu>
+        <el-submenu index="1-2">
+          <template slot="title">优先级</template>
+          <el-menu-item index="1-2-1">选项1</el-menu-item>
+          <el-menu-item index="1-2-2">选项2</el-menu-item>
+          <el-menu-item index="1-2-3">选项3</el-menu-item>
         </el-submenu>
       </el-submenu>
       <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <span slot="title">导航二</span>
+        <i class="el-icon-edit"></i>
+        <span slot="title">新建todo</span>
       </el-menu-item>
-      <el-menu-item index="3" disabled>
-        <i class="el-icon-document"></i>
-        <span slot="title">导航三</span>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <i class="el-icon-setting"></i>
-        <span slot="title">导航四</span>
+      <el-menu-item index="3">
+        <i class="el-icon-s-tools"></i>
+        <span slot="title">标签设置</span>
       </el-menu-item>
     </el-menu>
     </div>
     <div class="right-list">
       <el-table
-    :data="tableData"
-    style="width: 100%">
-    <el-table-column
-      label="日期"
-      width="180">
-      <template slot-scope="scope">
-        <i class="el-icon-time"></i>
-        <span style="margin-left: 10px">{{ scope.row.date }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column
-      label="姓名"
-      width="180">
-      <template slot-scope="scope">
-        <el-popover trigger="hover" placement="top">
-          <p>姓名: {{ scope.row.name }}</p>
-          <p>住址: {{ scope.row.address }}</p>
-          <div slot="reference" class="name-wrapper">
-            <el-tag size="medium">{{ scope.row.name }}</el-tag>
-          </div>
-        </el-popover>
-      </template>
-    </el-table-column>
-    <el-table-column label="操作">
-      <template slot-scope="scope">
-        <el-button
-          size="mini"
-          @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-        <el-button
-          size="mini"
-          type="danger"
-          @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+        :data="tableData"
+        style="width: 100%">
+        <el-table-column
+          label="截止日期"
+          width="180">
+          <template slot-scope="scope">
+            <i class="el-icon-time"></i>
+            <span style="margin-left: 10px">{{ scope.row.date }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="内容">
+          <template slot-scope="scope">
+            <span>{{ scope.row.text }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column 
+          label="标签"
+          width="180">
+          <template slot-scope="scope">
+            <el-tag class="tags" v-for="item in scope.row.tags" :key="item">{{item}}</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column 
+          label="操作"
+          width="180">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </div>
   </div>
 </template>
@@ -86,19 +84,68 @@ export default {
       tableData: [{
           date: '2016-05-02',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
+          text: '上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄',
+          tags: ['标签1','标签2','标签3']
         }, {
           date: '2016-05-04',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
+          text: '上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄',
+          tags: ['标签1','标签2']
         }, {
           date: '2016-05-01',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
+          text: '上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄',
+          tags: ['标签2','标签3']
         }, {
           date: '2016-05-03',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
+          text: '上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄',
+          tags: ['标签3']
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          text: '上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄',
+          tags: ['标签3']
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          text: '上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄',
+          tags: ['标签3']
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          text: '上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄',
+          tags: ['标签3']
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          text: '上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄',
+          tags: ['标签3']
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          text: '上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄',
+          tags: ['标签3']
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          text: '上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄',
+          tags: ['标签3']
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          text: '上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄',
+          tags: ['标签3']
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          text: '上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄',
+          tags: ['标签3']
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          text: '上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄',
+          tags: ['标签3']
         }]
     };
   },
@@ -113,6 +160,12 @@ export default {
   display: flex;
   .left-menu{
     width: 200px;
+  }
+  .right-list{
+    width: 1080px;
+    .tags{
+      margin: 0 3px;
+    }
   }
 }
 </style>
